088592c02d9903f566fafb80668c5c4540c9d4a8
JONGYUNCHOI <winboy0208@gmail.com>
2018-11-04 19:59:55 +0900

    no message

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sitemap
 
 if __name__ == '__main__':
 
-    logger = log.jlog()
+    logger = log.jlog().jongdalogger()
     step = logger.jongdalogger.info("1.Crawl start 2.View crawl site 3.Quit")
     step = int(input())
     if (step == 1):
