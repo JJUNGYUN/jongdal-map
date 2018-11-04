17041a93151dbc5a4d3f734d505c0ec87b27f6a2
JONGYUNCHOI <winboy0208@gmail.com>
2018-11-04 20:02:07 +0900

    no message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sitemap
 if __name__ == '__main__':
 
     logger = log.jlog().jongdalogger
-    step = logger.jongdalogger.info("1.Crawl start 2.View crawl site 3.Quit")
+    step = logger.info("1.Crawl start 2.View crawl site 3.Quit")
     step = int(input())
     if (step == 1):
         depth = int(input('Insert crawl depth : '))
